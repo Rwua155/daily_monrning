2c0a0231a103587843e0e041a24f6b06bcf671b7
快落 <847770212@qq.com>
2023-02-11 14:11:30 +0800

    2

--- main.py
+++ main.py
@@ -19,27 +19,27 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=深圳"
+    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city="+city
     res = requests.get(url).json()
 
     code = res['code']
     if code == 0:
         toDayWeather = res['data']['list'][0]
         toMorrowWeather = res['data']['list'][1]
-        datq = toDayWeather['weather'];
-        mrtq = toMorrowWeather['weather'];
+        datq = toDayWeather['weather']
+        mrtq = toMorrowWeather['weather']
         dqtqd = str(math.floor(toDayWeather['low']))+'℃'
         dqtqg = str(math.floor(toDayWeather['high']))+'℃'
         mrtqd = str(math.floor(toMorrowWeather['low']))+'℃'
         mrtqg = str(math.floor(toMorrowWeather['high']))+'℃'
-           
+
     else:
         url = "https://restapi.amap.com/v3/weather/weatherInfo?key=26111970b11fc6e5141d2de555e40f36&city=440300&extensions=all&output=JSON"
         res = requests.get(url).json()
         toDayWeather = res['forecasts'][0]['casts'][0]
         toMorrowWeather = res['forecasts'][0]['casts'][1]
-        datq = toDayWeather['dayweather'];
-        mrtq = toMorrowWeather['dayweather'];
+        datq = toDayWeather['dayweather']
+        mrtq = toMorrowWeather['dayweather']
         dqtqd = str(toDayWeather['nighttemp'])+'℃'
         dqtqg = str(toDayWeather['daytemp'])+'℃'
         mrtqd = str(toMorrowWeather['nighttemp'])+'℃'
@@ -51,31 +51,35 @@ def get_weather():
 
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
+
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    next = datetime.strptime(str(date.today().year) +
+                             "-" + birthday, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
+
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
+
 
 def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, wea1,lastTime,lastTime1 = get_weather()
-data = {"weather":{"value":wea,"color":get_random_color()},"lastTime":{"value":lastTime,"color":get_random_color()},
-        "weather1":{"value":wea1,"color":get_random_color()},"lastTime1":{"value":lastTime1,"color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, wea1, lastTime, lastTime1 = get_weather()
+data = {"weather": {"value": wea, "color": get_random_color()}, "lastTime": {"value": lastTime, "color": get_random_color()},
+        "weather1": {"value": wea1, "color": get_random_color()}, "lastTime1": {"value": lastTime1, "color": get_random_color()}, "words": {"value": get_words(), "color": get_random_color()}}
 
 
 res = wm.send_template(user_id, template_id, data)
